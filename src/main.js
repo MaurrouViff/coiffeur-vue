a1bb6cec87a3224e35f8efbc752c3559aacd44e7
Aurélien <rofldu08@gmail.com>
2024-01-08 16:03:51 +0100

    Fichier js qui gère les routes

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 // Liste des composants
 import Accueil from './components/pages/Accueil.vue';
+import Contact from './components/pages/Contact.vue';
+import Horraire from './components/pages/Horraire.vue';
+import Propos from './components/pages/Propos.vue';
 
-
+// Liste des routes
 const routes = [
     { path: '/', component: Accueil },
     { path: '/accueil', component: Accueil },
+    { path: '/contact', component: Contact },
+    { path: '/horraire', component: Horraire },
+    { path: '/propos', component: Propos },
 ];
 
 const router = createRouter({
