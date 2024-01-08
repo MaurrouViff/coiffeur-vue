7a23aee4b4a457c8c4d7ddc510e020335ac9df19
Aurélien <rofldu08@gmail.com>
2024-01-08 16:02:37 +0100

    Composent pour le haut de page

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,35 +1,34 @@
-<script setup>
-
-</script>
-
 <template>
 <div>
   <header>
     <nav class="navbar">
       <ul class="nav-menu">
+        <h2>Salon Nuances</h2>
         <li class="nav-item"><router-link to="/accueil" class="link-button">ACCUEIL</router-link></li>
-        <li class="nav-item"><router-link to="/accueil" class="link-button">ACCUEIL</router-link></li>
-        <li class="nav-item"><router-link to="/accueil" class="link-button">ACCUEIL</router-link></li>
-        <li class="nav-item"><router-link to="/accueil" class="link-button">ACCUEIL</router-link></li>
+        <li class="nav-item"><router-link to="/contact" class="link-button">CONTACT</router-link></li>
+        <li class="nav-item"><router-link to="/horraire" class="link-button">HORRAIRE</router-link></li>
+        <li class="nav-item"><router-link to="/propos" class="link-button">À PROPOS</router-link></li>
       </ul>
     </nav>
   </header>
+  <hr>
 </div>
 </template>
 
 <style scoped>
 .link-button {
-  color: #ffffff;
+  color: #000000;
   padding: 20px 30px;
   border: 1px solid;
   border-radius: 5px;
   cursor: pointer;
   font-size: 16px;
   text-decoration: none;
-  opacity: 0.8;
+  transition: background-color 0.3s, color 0.3s;
 }
 .link-button:hover {
-  opacity: 1;
+  background-color: black;
+  color: white;
 }
 .navbar {
   min-height: 12vh;
@@ -37,7 +36,7 @@
   justify-content: center;
   align-items: center;
   padding: 0 24px;
-  background-color: #000000;
+  background-color: #ffffff;
 }
 .nav-menu {
   display: flex;
@@ -52,5 +51,14 @@ li {
 header {
   text-align: center;
   justify-content: center;
+  background-color: #ffffff;
+}
+h2 {
+  color: #000000;
+}
+hr {
+  height: 5px;
+  border: 0;
+  background-color: black;
 }
 </style>
