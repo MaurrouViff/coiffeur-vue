f2977cd99e637ab5e1d1c57a6c256acd3570ce90
Aurélien <rofldu08@gmail.com>
2024-01-08 16:05:14 +0100

    Composent attention pour l'horraire

---
+++ src/components/WarningHorraire.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2 :class="{ 'animate-opacity': animateOpacity }" style="color: red;">⚠ ATTENTION ⚠</h2>
+    <h3>Nous ne travaillons ni le lundi ni le dimande.</h3>
+    <h3>Nous ne travaillons également en aucun cas les jours fériés.</h3>
+  </div>
+</template>
+<style scoped>
+@keyframes fadeInOut {
+  0%, 100% {
+    opacity: 0.1;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+.animate-opacity {
+  animation: fadeInOut 3s infinite;
+}
+
+div {
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      animateOpacity: true,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.animateOpacity = !this.animateOpacity;
+    }, 3000);
+  },
+};
+</script>
