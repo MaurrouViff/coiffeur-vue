3d6018d06db5a8261e2b43ff9b372e4bfd601a0c
Aurélien <rofldu08@gmail.com>
2024-01-08 11:17:32 +0100

    fichier js qui gère les routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// Liste des composants
+import Accueil from './components/pages/Accueil.vue';
 
-createApp(App).mount('#app')
+
+const routes = [
+    { path: '/', component: Accueil },
+    { path: '/accueil', component: Accueil },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+const app = createApp(App);
+
+
+app.use(router);
+
+
+app.mount('#app');
