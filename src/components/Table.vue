1bf64430e793fc5f9695d5ecf314befb097a1e13
Aurélien <rofldu08@gmail.com>
2024-01-08 16:04:18 +0100

    Composent qui gère le tableau des horraires

---
+++ src/components/Table.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <table>
+      <thead>
+      <tr>
+        <th>Nos horraires</th>
+        <th>Mardi</th>
+        <th>Mercredi</th>
+        <th>Jeudi</th>
+        <th>Vendredi</th>
+        <th>Samedi</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>8h - 9h</td>
+        <td>X</td>
+        <td>X</td>
+        <td>X</td>
+        <td>X</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>9h - 10h</td>
+        <td>✔</td>
+        <td>X</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>10h - 11h</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>11h - 12h</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>12h - 13h</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>13h - 14h</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>14h - 15h</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>15h - 16h</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td>17h - 18h</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>X</td>
+      </tr>
+      <tr>
+        <td>18h - 19h</td>
+        <td>✔</td>
+        <td>X</td>
+        <td>X</td>
+        <td>✔</td>
+        <td>X</td>
+      </tr>
+      <tr>
+        <td>19h - 20h</td>
+        <td>X</td>
+        <td>X</td>
+        <td>X</td>
+        <td>✔</td>
+        <td>X</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+table {
+  border-collapse: separate;
+  border-spacing: 0;
+  border-radius: 10px;
+  margin: 0 auto;
+  background-color: black;
+  color: white;
+
+}
+
+td, th {
+  border: 2px solid rgba(255, 255, 255, 0.5);
+  padding: 8px;
+  box-shadow: 0 0 0 1px rgb(0, 0, 0);
+  margin: -2px;
+}
+th {
+  color: rgba(255, 255, 255, 0.69);
+}
+</style>
