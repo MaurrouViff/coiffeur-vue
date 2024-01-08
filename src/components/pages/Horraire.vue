9690beb27dfd69ed9f87ea29f13fe6d980f44b70
Aurélien <rofldu08@gmail.com>
2024-01-08 16:06:02 +0100

    Page des horraires

---
+++ src/components/pages/Horraire.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <WarningHorraire />
+  <hr>
+  <Tableau />
+</div>
+</template>
+<script>
+import Tableau from "../Table.vue";
+import WarningHorraire from "../WarningHorraire.vue";
+
+
+export default {
+  components: {
+    Tableau,
+    WarningHorraire
+  }
+};
+</script>
+<style scoped>
+hr {
+  height: 5px;
+  border: 0;
+  background-color: black;
+}
+</style>
