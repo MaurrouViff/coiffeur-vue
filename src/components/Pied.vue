f949be51cc565a5f53d6649a1bfefdd8e21395d7
MaurrouViff <anawpdu08@gmail.com>
2024-01-11 08:35:27 +0100

    Ajout du pied pour le site

---
+++ src/components/Pied.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <footer>
+    <p>Ce site web appartient au Salon Nuances.</p>
+  </footer>
+</div>
+</template>
+
+<style scoped>
+p {
+  color: #ffffff;
+  margin: 0;
+}
+footer {
+  background-color: black;
+  min-height: 7vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+</style>
