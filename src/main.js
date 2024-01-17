1e94f843c84942acdd1d84a1f76ec8ff91b75405
MaurrouViff <rofldu08@gmail.com>
2024-01-17 13:41:19 +0100

    Ajout de la route pour le personnel

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import Accueil from './components/pages/Accueil.vue';
 import Contact from './components/pages/Contact.vue';
 import Horraire from './components/pages/Horraire.vue';
 import Propos from './components/pages/Propos.vue';
+import Personnel from './components/pages/Personnel.vue';
 
 // Liste des routes
 const routes = [
@@ -15,6 +16,7 @@ const routes = [
     { path: '/contact', component: Contact },
     { path: '/horraire', component: Horraire },
     { path: '/propos', component: Propos },
+    { path: '/personnel', component: Personnel }
 ];
 
 const router = createRouter({
