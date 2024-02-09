9fdd9fd602cdb0bc1b0545ab8d261077a7e04d91
MaurrouViff <rofldu08@gmail.com>
2024-02-09 17:17:29 +0100

    Modification des la page "à propos"

--- src/components/pages/Propos.vue
+++ src/components/pages/Propos.vue
@@ -3,9 +3,22 @@
   <p>Ce site web appartient au <span>Salon Nuances</span>.</p>
   <p>Situé à Charleville-Mézières, <a href="https://www.google.fr/maps/@49.7396183,4.7186805,3a,75y,43.47h,94.16t/data=!3m6!1e1!3m4!1stP2qdgXiNDWO8GSMOqC0eQ!2e0!7i16384!8i8192?entry=ttu" class="link-to-page">cliquez ici</a> pour voir où il se trouve ou cliquez sur le bouton "<span>CONTACT</span>".</p>
 </div>
+  <div>
+    <Tarifs />
+  </div>
+
+
 </template>
 <script>
+import Tarifs from "../Tarifs.vue";
 
+
+
+export default {
+  components: {
+    Tarifs,
+  }
+};
 </script>
 <style scoped>
 div {
