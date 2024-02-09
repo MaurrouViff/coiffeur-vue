00cd261c7cf338cca9ee922166ef24e57d3a899b
MaurrouViff <rofldu08@gmail.com>
2024-02-09 12:51:37 +0100

    création de la galerie

---
+++ src/components/pages/Galerie.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+ <Photos />
+</div>
+</template>
+
+<script>
+import Photos from '../Photos.vue';
+
+export default {
+  components: {
+    Photos
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
