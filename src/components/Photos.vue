ed225c4708c436f961a04eb8696088f06d6e38b2
MaurrouViff <rofldu08@gmail.com>
2024-02-09 17:34:57 +0100

    Ajout des photos pour la galerie

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,13 +1,42 @@
-<script>
-
-</script>
-
 <template>
   <div>
-
+    <section>
+      <h2>Voici une galerie pour voir à quoi ressemble notre salon</h2>
+      <hr>
+    </section>
+    <section>
+      <img src="../assets/image/image-interieur1.jpg" alt="Image n°1 dans la galerie">
+    </section>
+    <section>
+      <img src="../assets/image/image-interieur2.jpg" alt="Image n°2 dans la galerie">
+    </section>
+    <section>
+      <img src="../assets/image/image-interieur3.jpg" alt="Image n°3 dans le galerie">
+    </section>
+    <section>
+      <img src="../assets/image/image-interieur4.jpg" alt="Image n°4 dans la galerie">
+    </section>
+    <section>
+      <img src="../assets/image/image-interieur5.jpg" alt="Image n°5 dans la galerie">
+    </section>
   </div>
 </template>
 
 <style scoped>
-
+h2 {
+  color: red;
+  text-align: center;
+}
+hr {
+  background-color: black;
+  border: 0;
+  height: 5px;
+}
+img {
+  width: 100%;
+  height: 1500px;
+}
+section {
+  margin-bottom: -5px;
+}
 </style>
